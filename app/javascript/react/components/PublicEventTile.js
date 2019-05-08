26fd492cb0a4bbb8d9a03be243a77e82f3fab851
MikeMaven <mikemaven@gmail.com>
2019-05-08 09:32:18 -0400

    added columns to user table

--- app/javascript/react/components/PublicEventTile.js
+++ app/javascript/react/components/PublicEventTile.js
@@ -3,11 +3,13 @@ import React from 'react'
 const PublicEventTile = props => {
   return(
     <div className="event-tile">
-      <img src={props.event_photo}/>
-      <div className="event-tile-info">
-        <h4>{props.title}</h4>
-        <p>{props.description}</p>
-        <p><strong>{props.location_name}</strong></p>
+      <div className="event-tile-content">
+        <img src={props.event_photo}/>
+        <div className="event-tile-info">
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+          <p><strong>{props.location_name}</strong></p>
+        </div>
       </div>
     </div>
   )
