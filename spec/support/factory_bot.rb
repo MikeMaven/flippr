02f980585ac74287ee721de47f787c359f5a8119
MikeMaven <mikemaven@gmail.com>
2019-05-08 10:59:17 -0400

    santized user registration inputs

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,10 +1,14 @@
 require 'factory_bot'
+require 'faker'
 
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    username { Faker::Internet.username(8) }
   end
 
 end
