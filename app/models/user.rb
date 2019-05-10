2c311a0c6edade649eeb5794908e323534f466f9
MikeMaven <mikemaven@gmail.com>
2019-05-10 15:49:53 -0400

    incorporated geocoding into event feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  reverse_geocoded_by :latitude, :longitude,
+    :address => :location
+  after_validation :reverse_geocode
 end
