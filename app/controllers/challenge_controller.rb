a260ccfb90e752cfea1765e861a10ce24e7acb5d
MikeMaven <mikemaven@gmail.com>
2019-05-18 07:05:14 -0400

    changed to plain text

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,5 +1,5 @@
-class ChallengeController < ApplicationController
+class ChallengeController < ActionController::Base
   def show
-    render text: "vyW7hcov8KmIG4LaogvY2CgifvuRdnaordTPt4YkQfE.o6XMk7drlrBnCFzdIUvR8cBiXCwKsAjE_QsTjARo3YI"
+    render plain: "vyW7hcov8KmIG4LaogvY2CgifvuRdnaordTPt4YkQfE.o6XMk7drlrBnCFzdIUvR8cBiXCwKsAjE_QsTjARo3YI"
   end
 end
